5b2d290ed807f62c2ead025045e9ce2da37bcda4
vadim.gumennyj <vadim.gumennyj@gmail.com>
2020-01-21 20:07:44 +0200

    add  last client changes

---
+++ server/lib/privat.js
@@ -0,0 +1,70 @@
+const Merchant = require('privatbank-api')
+const moment = require('moment')
+const privat = require('config').privat
+
+const DATE_FORMAT = 'DD.MM.YYYY'
+
+merchant = new Merchant({
+  id: privat.id,
+  password: privat.password,
+  country: 'UA',
+})
+
+const DATA = {
+  "response": {
+    "version": "1.0",
+    "merchant": {"id": "138922", "signature": "4644caa4abc09d14b1b382964261f94d9a71eabf"},
+    "data": {
+      "oper": "cmt",
+      "info": {
+        "statements": {
+          "status": "excellent",
+          "credit": "0.0",
+          "debet": "82.0",
+          "statement": [{
+            "card": "5363542306858664",
+            "appcode": "308375",
+            "trandate": "2020-01-21",
+            "trantime": "12:12:00",
+            "amount": "0.50 UAH",
+            "cardamount": "-0.50 UAH",
+            "rest": "502.30 UAH",
+            "terminal": "PrivatBank, CP980T00",
+            "description": "Перевод на свою «Копилку» 26*66. Сдача от округления расходов до 1 UAH."
+          }, {
+            "card": "5363542306858664",
+            "appcode": "082522",
+            "trandate": "2020-01-21",
+            "trantime": "12:09:00",
+            "amount": "79.87 UAH",
+            "cardamount": "-81.50 UAH",
+            "rest": "502.80 UAH",
+            "terminal": "RestoranTzOVVANTOSh, L1HM2QJE",
+            "description": "Ресторан: РЕСТОРАН &quot;ТРАТОРРИЯ ПАПАРАЦЦИ&quot;, Хмельницький, вул. Проскурвська, 30"
+          }]
+        }
+      }
+    }
+  }
+}
+
+const getBalance = (cardNumber = '5363542306858664', startDate, endDate) => {
+  const start = moment(startDate).format(DATE_FORMAT)
+  const end = moment(endDate).format(DATE_FORMAT)
+
+  return Promise.resolve(DATA)
+
+  // return merchant
+  //   .statement(cardNumber, start, end)
+  //   .then((res) => {
+  //     return JSON.parse(res)
+  //   })
+  //   .catch((e) => {
+  //     console.log(e)
+  //     return null
+  //   })
+}
+
+module.exports = {
+  getBalance,
+}
